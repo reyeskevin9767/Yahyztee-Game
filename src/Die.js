17dfd0d70e3e0a93bc5282bb63482f28c0e2875e
Kevin Reyes <reyeskevin9767@gmail.com>
2019-07-18 12:34:41 -0500

    Add files via upload

---
+++ src/Die.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+
+    static defaultProps = {
+      numberwords: ["one", "two", "three", "four", "five", "six"],
+      val: 5
+    };
+
+  constructor(props){
+    super(props);
+      this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(){
+    this.props.handleClick(this.props.idx)
+  }
+
+  render() {
+      const {numberwords, locked, val, disabled, rolling} = this.props;
+      let classes = `Die fas fa-dice-${numberwords[val - 1]} fa-5x `;
+      if(locked) classes += "Die-locked ";
+      if(rolling) classes += "Die-rolling";
+    return (
+      <i
+        className={classes}
+        onClick={this.handleClick}
+        disabled={disabled}
+      >
+      </i>
+    );
+  }
+}
+
+export default Die;
